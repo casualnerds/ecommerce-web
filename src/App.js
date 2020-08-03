c86b2fff924e41d2f15d51006824f9f92eef07c3
basyri <husain.basyri@gmail.com>
2020-08-03 23:38:23 +0700

    add list product

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
 import './App.css';
-import { Signin } from './pages/Signin/Signin.page';
+
+import { Product } from './pages/Product/Product.page';
+// import { Signin } from './pages/Signin/Signin.page';
 // import { Home } from './pages/Home/Home.page';
 // import { Navbar } from './modules/Navbar/Navbar.module';
 
 function App() {
   return (
     <div>
-      <Signin />
-      {/* <Navbar />
-      <Home /> */}
+      <Product />
+      {/* <Signin /> */}
+      {/* <Navbar /> */}
+      {/* <Home /> */}
     </div>
   );
 }
