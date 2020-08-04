6f4595b58ea18064791781c0c3d2efe1e7069371
tyogautomo <yoga.utomo27@gmail.com>
2020-08-04 13:19:24 +0700

    get rid unused vars

--- src/pages/Dashboard/Dashboard.page.jsx
+++ src/pages/Dashboard/Dashboard.page.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
 
 import styles from './Dasboard.module.css';
 import { Navbar } from '../../modules/Navbar/Navbar.module';
