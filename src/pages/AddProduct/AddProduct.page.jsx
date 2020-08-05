98f0573361ff1f62a413e203aa38bf7640e00bfe
tyogautomo <yoga.utomo27@gmail.com>
2020-08-05 21:25:29 +0700

    create modul input select

--- src/pages/AddProduct/AddProduct.page.jsx
+++ src/pages/AddProduct/AddProduct.page.jsx
@@ -2,12 +2,12 @@ import React, { Component } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import styles from "./AddProduct.module.css";
+import { InputSelect } from '../../modules/InputSelect/InputSelect.module';
 
 class AddProduct extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            categoryOptionShow: false,
             categories: [
                 'Sneaker',
                 'Boost',
@@ -17,7 +17,6 @@ class AddProduct extends Component {
                 'Basket',
                 'Cycling'
             ],
-            currentOptionsRef: {},
             choosedCategory: ''
         };
     }
@@ -26,69 +25,20 @@ class AddProduct extends Component {
         document.title = 'Patucool | Add Product';
     }
 
-    toggleCategoryOptions = () => {
-        this.setState(prev => ({
-            categoryOptionShow: !prev.categoryOptionShow,
-        }));
-    }
-
-    onBlurCategoryOptions = () => {
-        this.setState({ categoryOptionShow: false });
-    }
-
     onChooseCategory = category => () => {
         this.setState({ choosedCategory: category });
     }
 
-    renderSelectInput = () => {
-        const { categoryOptionShow, choosedCategory } = this.state;
+    renderInputSelect = () => {
+        const { categories, choosedCategory } = this.state;
 
         return (
-            <div
-                onClick={this.toggleCategoryOptions}
-                onBlur={this.onBlurCategoryOptions}
-                tabIndex="0" // nescesary for onFocus and onBlur
-                className={styles.selectable}
-            >
-                <div className={styles.selectableRowWrapper}>
-                    <p>{choosedCategory ? choosedCategory : 'Select an option ...'}</p>
-                    <FontAwesomeIcon
-                        icon="chevron-circle-down"
-                        className={`${styles.arrowDownIcon} ${categoryOptionShow ? styles.arrowDownIconExpand : null}`}
-                    />
-                </div>
-            </div>
-        );
-    }
-
-    renderCategoryOptionsModal = () => {
-        const {
-            categoryOptionShow,
-            currentOptionsRef,
-            categories,
-            choosedCategory
-        } = this.state;
-
-        return (
-            <div
-                className={`${styles.optionsContainer} ${categoryOptionShow ? styles.optionsContainerExpand : null}`}
-                style={{ minWidth: currentOptionsRef.width }}
-            >
-                <ul className={styles.listContainer}>
-                    {categories.map((category, i) => (
-                        <li key={i} className={styles.listCard} onClick={this.onChooseCategory(category)}>
-                            <div>
-                                {
-                                    category === choosedCategory
-                                        ? <FontAwesomeIcon icon="check" className={styles.checkIcon} />
-                                        : null
-                                }
-                            </div>
-                            <p>{category}</p>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+            <InputSelect
+                options={categories}
+                choosedOption={choosedCategory}
+                onChooseOption={this.onChooseCategory}
+                placeholder="Choose category .."
+            />
         );
     }
 
@@ -99,10 +49,7 @@ class AddProduct extends Component {
                 <h2 className={styles.inputTitle}>Product's Name:</h2>
                 <input type="text" />
                 <h2 className={styles.inputTitle}>Category:</h2>
-                <div style={{ position: 'relative' }}>
-                    {this.renderSelectInput()}
-                    {this.renderCategoryOptionsModal()}
-                </div>
+                {this.renderInputSelect()}
                 <h2 className={styles.inputTitle}>Product's Name:</h2>
                 <input type="text" />
             </div >
